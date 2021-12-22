d9f188767f77392a0ad374b27d5b7a89bd3f3efd
Jedn <jed@nelson.ac>
2021-12-22 17:25:38 +0000

    settings for indent blankline

--- neovim/.config/nvim/lua/plugins/indentline.lua
+++ neovim/.config/nvim/lua/plugins/indentline.lua
@@ -2,6 +2,8 @@ require("indent_blankline").setup {
   space_char_blankline = " ",
   show_current_context = true,
   show_current_context_start = true,
+  use_treesitter = true,
+  show_first_indent_level = false,
   filetype_exclude = {'help'},
   buftype_exclude = {'terminal'}
 }
