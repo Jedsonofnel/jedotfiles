a7aec48131a208b8b030569d80b9a6c25c4493b0
jedn <jed@nelson.ac>
2022-08-02 11:50:20 +0200

    Add a whole bunch of options

--- neovim/.config/nvim/lua/jedn/set.lua
+++ neovim/.config/nvim/lua/jedn/set.lua
@@ -1,26 +1,32 @@
 local options = {
     termguicolors = true,
-
     hidden = true,
-
     clipboard = "unnamedplus",
     cmdheight = 1,
-
     nu = true,
     relativenumber = true,
-
     tabstop = 4,
     softtabstop = 4,
     shiftwidth = 4,
     expandtab = true,
-
     hlsearch = false,
     incsearch = true,
     smartcase = true,
     ignorecase = true,
-
     smartindent = true,
     wrap = false,
+    completeopt = { "menuone", "noselect" },
+    conceallevel = 0,
+    fileencoding = "utf-8",
+    showmode = false,
+    showtabline = 0,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    undofile = true,
+    cursorline = true,
+    signcolumn = "yes",
+    scrolloff = 8,
 }
 
 for k, v in pairs(options) do
