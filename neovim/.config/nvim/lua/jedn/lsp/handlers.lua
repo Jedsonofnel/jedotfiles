3f82003c6b58b328d515c94245a5b29166a20e2e
jedn <jed@nelson.ac>
2022-08-02 20:16:55 +0200

    Add lsp keybinds

--- neovim/.config/nvim/lua/jedn/lsp/handlers.lua
+++ neovim/.config/nvim/lua/jedn/lsp/handlers.lua
@@ -1,63 +1,70 @@
 -- lsp handler options
 
-local nnoremap_bufnr = require("jedn.utils").nnoremap_bufnr
+local nnoremap = require("jedn.utils").nnoremap
 local M = {}
 
 -- run at startup
 M.setup = function()
-    -- better diagnostic icons
-    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
+	-- better diagnostic icons
+	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	end
 
-    vim.diagnostic.config({
-        virtual_lines = false,
-        virtual_text = false,
-        signs = true,
-        underline = true,
-        severity_sort = true,
-        update_in_insert = true,
-        float = {
-            style = "minimal",
-            border = "rounded",
-            source = "if_many",
-            header = "",
-            prefix = "",
-        }
-    })
+	vim.diagnostic.config({
+		virtual_lines = false,
+		virtual_text = false,
+		signs = true,
+		underline = true,
+		severity_sort = true,
+		update_in_insert = true,
+		float = {
+			style = "minimal",
+			border = "rounded",
+			source = "if_many",
+			header = "",
+			prefix = "",
+		},
+	})
 end
 
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
+	vim.lsp.buf.format({
+		filter = function(client)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 M.on_attach = function(client, bufnr)
-    -- diagnostic stuff
-    nnoremap_bufnr(bufnr, "<leader>de", "<cmd>lua vim.diagnostic.open_float()<CR>")
-    nnoremap_bufnr(bufnr, "<leader>dp", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
-    nnoremap_bufnr(bufnr, "<leader>dn", "<cmd>lua vim.diagnostic.goto_next()<CR>")
-    nnoremap_bufnr(bufnr, "<leader>dq", "<cmd>lua vim.diagnostic.setloclist()<CR>")
+	-- diagnostic stuff
+	nnoremap("<leader>de", vim.diagnostic.open_float, { buffer = bufnr })
+	nnoremap("<leader>dp", vim.diagnostic.goto_prev, { buffer = bufnr })
+	nnoremap("<leader>dn", vim.diagnostic.goto_next, { buffer = bufnr })
+	nnoremap("<leader>dl", vim.diagnostic.setloclist, { buffer = bufnr })
 
-    -- formatting with null-ls only/on save
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
+	-- lsp stuff
+	nnoremap("<leader>ld", vim.lsp.buf.definition, { buffer = bufnr })
+	nnoremap("<leader>lD", vim.lsp.buf.declaration, { buffer = bufnr })
+	nnoremap("<leader>lf", function()
+		lsp_formatting(bufnr)
+	end, { buffer = bufnr })
+
+	-- formatting with null-ls only/on save
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
 end
 
 return M
--- neovim/.config/nvim/lua/jedn/utils.lua
+++ neovim/.config/nvim/lua/jedn/utils.lua
@@ -2,14 +2,11 @@ local M = {}
 
 -- keybind utility functions
 local function bind(op, outer_opts)
-    outer_opts = outer_opts or { noremap = true }
-    return function(lhs, rhs, opts)
-        opts = vim.tbl_extend("force",
-            outer_opts,
-            opts or {}
-        )
-        vim.keymap.set(op, lhs, rhs, opts)
-    end
+	outer_opts = outer_opts or { noremap = true }
+	return function(lhs, rhs, opts)
+		opts = vim.tbl_extend("force", outer_opts, opts or {})
+		vim.keymap.set(op, lhs, rhs, opts)
+	end
 end
 
 M.nmap = bind("n", { noremap = false })
@@ -19,32 +16,18 @@ M.xnoremap = bind("x")
 M.inoremap = bind("i")
 M.tnoremap = bind("t")
 
--- bind to a specific buffer
-local function bind_bufnr(op, outer_opts)
-    outer_opts = outer_opts or { noremap = true, silent = true }
-    return function(buffer, lhs, rhs, opts)
-        opts = vim.tbl_extend("force",
-            outer_opts,
-            opts or {}
-        )
-        vim.api.nvim_buf_set_keymap(buffer, op, lhs, rhs, opts)
-    end
-end
-
-M.nnoremap_bufnr = bind_bufnr("n")
-
 -- autocommand util function
 -- copied from https://github.com/norcalli/nvim_utils
 M.create_augroups = function(definitions)
-    for group_name, definition in pairs(definitions) do
-        vim.api.nvim_command('augroup ' .. group_name)
-        vim.api.nvim_command('autocmd!')
-        for _, def in ipairs(definition) do
-            local command = table.concat(vim.tbl_flatten { 'autocmd', def }, ' ')
-            vim.api.nvim_command(command)
-        end
-        vim.api.nvim_command('augroup END')
-    end
+	for group_name, definition in pairs(definitions) do
+		vim.api.nvim_command("augroup " .. group_name)
+		vim.api.nvim_command("autocmd!")
+		for _, def in ipairs(definition) do
+			local command = table.concat(vim.tbl_flatten({ "autocmd", def }), " ")
+			vim.api.nvim_command(command)
+		end
+		vim.api.nvim_command("augroup END")
+	end
 end
 
 return M
