812caa365365ef32dc38732e30e38c0bf737b20e
jedn <jed@nelson.ac>
2022-08-05 15:15:00 +0200

    Move options to plugins/options

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,6 +1,14 @@
 -- jedn and his nvim config
 
-require("jedn.set")
+-- leader key
+vim.g.mapleader = " "
+
+-- Neovide specific stuff (how I mostly use neovim)
+if vim.g.neovide then
+  vim.opt.clipboard = "" -- breaks linewise pasting :(
+  vim.opt.guifont = "FiraCode NF"
+end
+
 require("jedn.mappings")
 require("jedn.autocmds")
 require("jedn.lsp")
---
+++ neovim/.config/nvim/plugin/options.lua
@@ -0,0 +1,96 @@
+-- options!
+
+local opt = vim.opt
+
+opt.showmode = false
+opt.showcmd = true
+opt.cmdheight = 1
+opt.incsearch = true -- makes search better ...?
+opt.showmatch = true -- show matching bracket
+opt.relativenumber = true -- show rel numbers
+opt.number = true -- show actual number as well
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- unless capital is used
+opt.hidden = true -- buffers stay when hidden
+opt.equalalways = false
+opt.splitright = true
+opt.splitbelow = true
+opt.updatetime = 1000
+opt.hlsearch = true
+opt.scrolloff = 8
+
+-- only have it in the active buffer
+-- copied from: https://github.com/tjdevries/config_manager/
+opt.cursorline = true
+local group =
+  vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
+local set_cursorline = function(event, value, pattern)
+  vim.api.nvim_create_autocmd(event, {
+    group = group,
+    pattern = pattern,
+    callback = function()
+      vim.opt_local.cursorline = value
+    end,
+  })
+end
+-- when event, show cursor?
+set_cursorline("WinLeave", false)
+set_cursorline("WinEnter", true)
+set_cursorline("FileType", false, "TelescopePrompt")
+
+-- tabs!
+opt.autoindent = true
+opt.cindent = true -- indenting rules based on C, not really sure...
+opt.wrap = true
+
+opt.tabstop = 4 -- number of whitespace for \t char
+opt.shiftwidth = 4 -- width of indentation
+opt.softtabstop = 4 -- number of space added by pressing tab button
+opt.expandtab = true -- \t -> whitespace
+
+opt.breakindent = true -- wrapped lines will indent!
+opt.showbreak = string.rep(" ", 3) -- wrapped lines will have this appended to
+opt.linebreak = true -- wraps at word not character
+
+opt.foldmethod = "marker"
+opt.foldlevel = 0
+opt.modelines = 1
+
+opt.belloff = "all"
+
+opt.clipboard = "unnamedplus"
+
+opt.inccommand = "split" -- todo with cmd previews
+opt.swapfile = false
+
+-- shared data
+-- ! = save and restore global variables
+-- ' = max number of marked files remembered
+-- < = max number of lines saved for each register
+-- s = max size of item's content in KiB
+opt.shada = { "!", "'1000", "<50", "s10" }
+
+opt.mouse = "n"
+
+opt.formatoptions = opt.formatoptions
+  - "a" -- autoformatting of paragraphs NO
+  - "t" -- autowrap using textwidth NO
+  + "c" -- autowrap comments
+  + "q" -- allow formatting of comments with "gq"
+  - "o" -- automatically insert comment leader after "o" NO
+  + "r" -- automatically continue comment with <enter> tho
+  + "n" -- formats numbered lists with good indentation
+  + "j" -- auto-remove comments when joining lines
+  - "2" -- uses 2nd line of paragraph for indentation!?!?
+
+opt.joinspaces = false -- this option makes a . insert two spaces
+
+opt.fillchars = { eob = "~" }
+
+opt.diffopt = {
+  "internal",
+  "filler",
+  "closeoff",
+  "hiddenoff",
+  "algorithm:minimal",
+}
