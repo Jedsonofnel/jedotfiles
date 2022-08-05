f795cee5bb14453c6343d8d8737056f57e901fe2
jedn <jed@nelson.ac>
2022-08-05 22:00:57 +0200

    Format this file

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -3,36 +3,36 @@
 local actions = require("telescope.actions")
 
 require("telescope").setup({
-	defaults = {
-		vimgrep_arguments = {
-			"rg",
-			"--hidden",
-			"--color-never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-		},
-		mappings = {
-			i = {
-				["<esc>"] = actions.close,
-				["<c-j>"] = actions.move_selection_next,
-				["<c-k>"] = actions.move_selection_previous,
-			},
-		},
-		file_ignore_patterns = {
-			"node_modules",
-		},
-	},
-	extensions = {
-		fzf = {
-			fuzzy = true,
-			override_generic_sorter = true,
-			override_file_sorter = true,
-			case_mode = "smart_case",
-		},
-	},
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--hidden",
+      "--color-never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+    },
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<c-j>"] = actions.move_selection_next,
+        ["<c-k>"] = actions.move_selection_previous,
+      },
+    },
+    file_ignore_patterns = {
+      "node_modules",
+    },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    },
+  },
 })
 
 -- for blazingly fast sorting!
@@ -46,12 +46,12 @@ require("telescope").load_extension("projects")
 local nnoremap = require("jedn.utils").nnoremap
 
 nnoremap(
-	"<leader>ff",
-	"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>"
+  "<leader>ff",
+  "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>"
 )
 nnoremap(
-	"<leader> ",
-	"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>"
+  "<leader> ",
+  "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>"
 )
 nnoremap("<leader>fg", "<cmd>Telescope live_grep<cr>")
 nnoremap("<leader>fb", "<cmd>Telescope buffers<cr>")
