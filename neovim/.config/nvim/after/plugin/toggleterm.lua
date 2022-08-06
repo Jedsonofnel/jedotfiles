b4c4b2c8cf0ced668bcea46ffd85a4ee3d5ef3dd
jedn <jed@nelson.ac>
2022-08-06 15:38:28 +0200

    Implement rose-pine toggleterm highlights

--- neovim/.config/nvim/after/plugin/toggleterm.lua
+++ neovim/.config/nvim/after/plugin/toggleterm.lua
@@ -1,22 +1,26 @@
 -- toggleterm config
 
 local nnoremap = require("jedn.utils").nnoremap
+local highlights = require("rose-pine.plugins.toggleterm")
 
 require("toggleterm").setup({
-	size = 15,
-	persist_size = false,
-	hide_numbers = true,
-	shade_terminals = true,
-	direction = "horizontal",
+  size = 15,
+  persist_size = false,
+  hide_numbers = true,
+  shade_terminals = true,
+  direction = "horizontal",
+  highlights = highlights,
 
-	open_mapping = "<leader>tt",
-	start_in_insert = true,
-	insert_mappings = false,
-	terminal_mappings = false,
+  open_mapping = "<leader>tt",
+  start_in_insert = true,
+  insert_mappings = false,
+  terminal_mappings = false,
 
-	close_on_exit = true,
+  close_on_exit = true,
 })
 
 nnoremap("<leader>g", function()
-	require("toggleterm.terminal").Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" }):toggle()
+  require("toggleterm.terminal").Terminal
+    :new({ cmd = "lazygit", hidden = true, direction = "float" })
+    :toggle()
 end, { silent = true })
