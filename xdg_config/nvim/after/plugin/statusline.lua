640ac754809e23c4014adb1744ad982fee9462db
jedn <jed@nelson.ac>
2022-08-06 16:51:31 +0200

    remove hopefully the correct bits of lualine config

--- xdg_config/nvim/after/plugin/statusline.lua
+++ xdg_config/nvim/after/plugin/statusline.lua
@@ -5,15 +5,5 @@ require("lualine").setup({
 
     section_separators = { left = "", right = "" },
     component_separators = { left = "|", right = "|" },
-
-    refresh = {
-      statusline = 1000,
-    },
-  },
-  winbar = {
-    lualine_c = { "filename" },
-  },
-  inactive_winbar = {
-    lualine_c = { "filename" },
   },
 })
