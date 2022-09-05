a4b9597b31150948a9a24e0f6ef0787704f01229
Jed Nelson <jed@nelson.ac>
2022-09-05 11:57:39 +0100

    Remove context highlighting because it annoyed me

--- xdg_config/nvim/after/plugin/indent_blankline.lua
+++ xdg_config/nvim/after/plugin/indent_blankline.lua
@@ -7,12 +7,11 @@ vim.opt.fillchars = { eob = "~" }
 
 require("indent_blankline").setup({
   char = "▏",
-  context_char = "▏",
   use_treesitter = true,
   show_trailing_blankline_indent = false,
   show_end_of_line = true,
-  show_current_context = true,
-  show_current_context_start = true,
+  show_current_context = false,
+  show_current_context_start = false,
 
   filetype_exclude = {
     "netrw",
