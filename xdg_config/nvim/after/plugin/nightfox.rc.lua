25977bb617e5e798d79615d77aa71862bd19473e
jedn <jed@nelson.ac>
2022-12-24 13:40:45 +0100

    Separate generic color settings from colorscheme settings

--- xdg_config/nvim/after/plugin/nightfox.rc.lua
+++ xdg_config/nvim/after/plugin/nightfox.rc.lua
@@ -21,13 +21,3 @@ nightfox.setup({
     },
   },
 })
-
-local function ColorSettings(color)
-  color = color or "nightfox"
-  vim.cmd.colorscheme(color)
-
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-ColorSettings()
