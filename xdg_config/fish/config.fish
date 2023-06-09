a0cfdc66b2d6b486216d3621a0c1023945b237aa
Jed Nelson <jed@nelson.ac>
2023-06-09 17:19:22 +0100

    faffing around with fish for nvim config switching

--- xdg_config/fish/config.fish
+++ xdg_config/fish/config.fish
@@ -8,9 +8,25 @@ alias nvimrc "cd ~/jedotfiles/xdg_config/nvim"
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 
-# asdf (version manager for ruby and js)
-if test -f ~/.asdf/asdf.fish
-    source ~/.asdf/asdf.fish
+# neovim switcher
+# function nvims
+#     set items default AstroNvim nyoom
+#     set config (printf "%s\n" $items | fzf --prompt=" Neovim Config >> " --height=~50% --layout=reverse --border --exit-0)
+#     if test -z $config
+#         echo "Nothing selected"
+#         return 0
+#     else if test $config = default
+#         set $config ""
+#     end
+#     NVIM_APPNAME=$config nvim $argv
+# end
+
+# Nyoom config file location:
+# set -gx NYOOM_APPNAME nyoom
+
+# Nyoom binaries
+if test -f $XDG_CONFIG_HOME/$NYOOM_APPNAME
+    fish_add_path $XDG_CONFIG_HOME/$NYOOM_APPNAME/bin
 end
 
 # tmuxinator stuff
@@ -42,15 +58,17 @@ if test -f ~/.local/bin
     fish_add_path ~/.local/bin
 end
 
-# theme etc
-source ~/.config/fish/kanagawa.fish
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /home/jedn/.asdf/installs/python/miniconda3-latest/bin/conda
     eval /home/jedn/.asdf/installs/python/miniconda3-latest/bin/conda "shell.fish" hook $argv | source
 end
 # <<< conda initialize <<<
+#
+# asdf (version manager for ruby and js)
+if test -f ~/.asdf/asdf.fish
+    source ~/.asdf/asdf.fish
+end
 
 # starship (prompt)
 starship init fish | source
---
+++ xdg_config/fish/functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/jedn/.fzf/shell/key-bindings.fish
