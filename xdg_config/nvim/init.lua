9b3e0db8febf142e88f6a510119ea09f43350951
Jed Nelson <jed@nelson.ac>
2023-10-06 19:51:50 +0100

    Move lazy stuff out of init.lua

--- xdg_config/nvim/init.lua
+++ xdg_config/nvim/init.lua
@@ -6,29 +6,10 @@ vim.g.mapleader = " "
 -- global options
 require("jedn.options")
 
--- lazy plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- plugins
-require("lazy").setup("jedn.plugins", {
-  install = {
-    colorscheme = { "kanagawa" },
-  },
-  change_detection = {
-    notify = false,
-  },
-})
+require("jedn.lazy")
 
 -- keymaps (last because we are using which-key)
+-- eventually any which-key keymaps should be moved to which-key.lua
+-- in plugins
 require("jedn.keymaps")
