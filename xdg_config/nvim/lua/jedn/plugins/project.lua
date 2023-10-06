836987afb608e0211a09030e4c75e678c653e4d6
Jed Nelson <jed@nelson.ac>
2023-10-06 21:27:46 +0100

    Add stylua.lua to project.nvim to make nvimrc a project root

--- xdg_config/nvim/lua/jedn/plugins/project.lua
+++ xdg_config/nvim/lua/jedn/plugins/project.lua
@@ -4,9 +4,16 @@ return {
     opts = {
       manual_mode = false,
       show_hidden = true,
+      patterns = {
+        ".git",
+        "_darcs",
+        ".hg",
+        ".bzr",
+        ".svn",
+        "Makefile",
+        "package.json",
+        "stylua.toml",
+      },
     },
-    config = function(_, opts)
-      require("project_nvim").setup(opts)
-    end,
   },
 }
