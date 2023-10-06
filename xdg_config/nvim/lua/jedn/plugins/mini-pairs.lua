9ccd0e5ca67a0c4b204578728a12cba0a2fec42b
Jed Nelson <jed@nelson.ac>
2023-10-06 22:03:30 +0100

    Expanded config

--- xdg_config/nvim/lua/jedn/plugins/mini-pairs.lua
+++ xdg_config/nvim/lua/jedn/plugins/mini-pairs.lua
@@ -1,9 +1,37 @@
 return {
-  {
-    "echasnovski/mini.pairs",
-    event = "VeryLazy",
-    config = function(_, opts)
-      require("mini.pairs").setup(opts)
-    end,
+  "echasnovski/mini.pairs",
+  event = "VeryLazy",
+  opts = {
+    mappings = {
+      ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+      ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+      ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+
+      [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+      ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+      ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+
+      ['"'] = {
+        action = "closeopen",
+        pair = '""',
+        neigh_pattern = "[^\\].",
+        register = { cr = false },
+      },
+      ["'"] = {
+        action = "closeopen",
+        pair = "''",
+        neigh_pattern = "[^%a\\].",
+        register = { cr = false },
+      },
+      ["`"] = {
+        action = "closeopen",
+        pair = "``",
+        neigh_pattern = "[^\\].",
+        register = { cr = false },
+      },
+    },
   },
+  config = function(_, opts)
+    require("mini.pairs").setup(opts)
+  end,
 }
