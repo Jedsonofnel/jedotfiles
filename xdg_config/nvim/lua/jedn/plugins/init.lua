efe1e2c3437a9cf05a965e477b06d887f85b6acf
Jed Nelson <jed@nelson.ac>
2023-10-06 18:54:43 +0100

    Rationalise treesitter stuff

--- xdg_config/nvim/lua/jedn/plugins/init.lua
+++ xdg_config/nvim/lua/jedn/plugins/init.lua
@@ -56,17 +56,11 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
     opts = {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
       },
-      indent = { enable = false },
-      context_commentstring = { enable = true, enable_autocmd = false },
       ensure_installed = "all",
     },
     config = function(_, opts)
