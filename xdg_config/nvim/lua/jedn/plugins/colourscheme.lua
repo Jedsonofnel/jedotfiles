4b0e2352a0e070778c5f6b5ae68cdff5562e4175
Jed Nelson <jed@nelson.ac>
2023-10-06 22:02:39 +0100

    Remove coloured number column

--- xdg_config/nvim/lua/jedn/plugins/colourscheme.lua
+++ xdg_config/nvim/lua/jedn/plugins/colourscheme.lua
@@ -5,36 +5,18 @@ return {
     priority = 1000,
     -- transparent floating windows (nicked from kanagawa repo)
     opts = {
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-
-          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = {
-            fg = theme.ui.fg_dim,
-            bg = theme.ui.bg_m1,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
           },
-          TelescopeResultsBorder = {
-            fg = theme.ui.bg_m1,
-            bg = theme.ui.bg_m1,
-          },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = {
-            bg = theme.ui.bg_dim,
-            fg = theme.ui.bg_dim,
-          },
-        }
-      end,
+        },
+      },
     },
-    config = function()
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
       vim.cmd.colorscheme("kanagawa")
     end,
   },
