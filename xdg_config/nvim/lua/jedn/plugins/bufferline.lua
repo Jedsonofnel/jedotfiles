1c09672984209c93b21122b5d6f248a82638a9ee
Jed Nelson <jed@nelson.ac>
2023-10-06 21:50:11 +0100

    Make bufferline work with nvim-tree

--- xdg_config/nvim/lua/jedn/plugins/bufferline.lua
+++ xdg_config/nvim/lua/jedn/plugins/bufferline.lua
@@ -1,31 +1,29 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-      {
-        "<leader>bP",
-        "<Cmd>BufferLineGroupClose ungrouped<CR>",
-        desc = "Delete non-pinned buffers",
-      },
-    },
-    opts = {
-      options = {
-        mode = "tabs",
-        diagnostics = "nvim_lsp",
-        always_show_bufferline = false,
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "Neo-tree",
-            highlight = "Directory",
-            text_align = "left",
-          },
+  "akinsho/bufferline.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    options = {
+      mode = "tabs",
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = true,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "Nvim-Tree",
+          highlight = "Directory",
+          text_align = "left",
         },
       },
+    },
+  },
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+    {
+      "<leader>bP",
+      "<Cmd>BufferLineGroupClose ungrouped<CR>",
+      desc = "Delete non-pinned buffers",
     },
   },
 }
