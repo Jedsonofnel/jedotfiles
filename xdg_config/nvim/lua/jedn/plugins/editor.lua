273227a34aba4a5982c6547f9005bdbff291d0f9
Jed Nelson <jed@nelson.ac>
2023-10-06 18:54:50 +0100

    Rationalize neotree

--- xdg_config/nvim/lua/jedn/plugins/editor.lua
+++ xdg_config/nvim/lua/jedn/plugins/editor.lua
@@ -140,52 +140,12 @@ return {
   -- neotree
   {
     "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
     cmd = "Neotree",
-    keys = {
-      {
-        "<leader>fe",
-        function()
-          require("neo-tree.command").execute({
-            toggle = true,
-            dir = vim.loop.cwd(),
-          })
-        end,
-        desc = "Explorer NeoTree (root dir)",
-      },
-    },
-
-    deactivate = function()
-      vim.cmd([[Neotree close]])
-    end,
-
-    init = function()
-      vim.g.neo_tree_remove_legacy_commands = 1
-      if vim.fn.argc() == 1 then
-        local stat = vim.loop.fs_stat(vim.fn.argv(0))
-        if stat and stat.type == "directory" then
-          require("neo-tree")
-        end
-      end
-    end,
-
-    opts = {
-      filesystem = {
-        bind_to_cwd = true,
-        follow_current_file = true,
-      },
-      window = {
-        mappings = {
-          ["<space>"] = "none",
-        },
-      },
-      default_component_configs = {
-        indent = {
-          with_expanders = true,
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
-        },
-      },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
     },
   },
 }
