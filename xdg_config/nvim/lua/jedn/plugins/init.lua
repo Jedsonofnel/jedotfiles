091e832f9ed4ffb645222b77664f185fee342831
Jed Nelson <jed@nelson.ac>
2023-10-06 18:27:31 +0100

    Colorscheme figuring out

--- xdg_config/nvim/lua/jedn/plugins/init.lua
+++ xdg_config/nvim/lua/jedn/plugins/init.lua
@@ -5,26 +5,51 @@ return {
   {
     "rebelot/kanagawa.nvim",
     lazy = false,
-    priority = 1001,
+    priority = 1000,
+    -- transparent floating windows (nicked from kanagawa repo)
+    opts = {
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+
+          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+          TelescopeTitle = { fg = theme.ui.special, bold = true },
+          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+          TelescopeResultsNormal = {
+            fg = theme.ui.fg_dim,
+            bg = theme.ui.bg_m1,
+          },
+          TelescopeResultsBorder = {
+            fg = theme.ui.bg_m1,
+            bg = theme.ui.bg_m1,
+          },
+          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+          TelescopePreviewBorder = {
+            bg = theme.ui.bg_dim,
+            fg = theme.ui.bg_dim,
+          },
+        }
+      end,
+    },
     config = function()
       vim.cmd.colorscheme("kanagawa")
     end,
   },
+
   {
     "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme("tokyonight")
-    -- end,
+    lazy = true,
   },
+
   {
     "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme("gruvbox")
-    -- end,
+    lazy = true,
   },
 
   -- Treesitter for syntax highlighting goodness!
