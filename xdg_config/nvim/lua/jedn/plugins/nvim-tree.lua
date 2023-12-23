ea41cec08c7bfa36fe63c7cda9beb6536bc4b5a3
Jed Nelson <jed@nelson.ac>
2023-12-23 12:28:43 +0100

    NVchat nvim-tree copy

--- xdg_config/nvim/lua/jedn/plugins/nvim-tree.lua
+++ xdg_config/nvim/lua/jedn/plugins/nvim-tree.lua
@@ -1,42 +1,102 @@
 return {
   "nvim-tree/nvim-tree.lua",
   depencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+
   opts = {
+    filters = {
+      dotfiles = false,
+      custom = { ".DS_Store" },
+    },
+
+    disable_netrw = true,
+    hijack_netrw = true,
+    hijack_cursor = true,
+    hijack_unnamed_buffer_when_opening = false,
+    sync_root_with_cwd = false,
+
+    update_focused_file = {
+      enable = true,
+      update_root = false,
+    },
+
     view = {
-      width = 35,
-      number = false,
+      adaptive_size = false,
+      side = "left",
+      width = 30,
+      preserve_window_proportions = true,
     },
-    renderer = {
-      indent_markers = {
-        enable = true,
-      },
-      icons = {
-        glyphs = {
-          folder = {},
-        },
-      },
+
+    git = {
+      enable = false,
+      ignore = true,
     },
+
+    filesystem_watchers = {
+      enable = true,
+    },
+
     -- disable window_picker for explorer to work well with splits
     actions = {
       open_file = {
+        resize_window = true,
         window_picker = {
           enable = false,
         },
       },
     },
-    filters = {
-      custom = { ".DS_Store" },
-    },
-    git = {
-      enable = false,
+
+    renderer = {
+      root_folder_label = false,
+      highlight_git = false,
+      highlight_opened_files = "none",
+
+      indent_markers = {
+        enable = false,
+      },
+
+      icons = {
+        show = {
+          file = true,
+          folder = true,
+          folder_arrow = true,
+          git = false,
+        },
+
+        glyphs = {
+          default = "󰈚",
+          symlink = "",
+          folder = {
+            default = "",
+            empty = "",
+            empty_open = "",
+            open = "",
+            symlink = "",
+            symlink_open = "",
+            arrow_open = "",
+            arrow_closed = "",
+          },
+          git = {
+            unstaged = "✗",
+            staged = "✓",
+            unmerged = "",
+            renamed = "➜",
+            untracked = "★",
+            deleted = "",
+            ignored = "◌",
+          },
+        },
+      },
     },
   },
+
   config = function(_, opts)
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
     require("nvim-tree").setup(opts)
   end,
+
   keys = {
     {
       "<leader>ee",
