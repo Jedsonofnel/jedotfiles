c9e8db7e571e518e11c723c1feb491836ab79456
Jed Nelson <jed@nelson.ac>
2023-12-23 21:06:21 +0100

    Nvim tree fix typo

--- xdg_config/nvim/lua/jedn/plugins/nvim-tree.lua
+++ xdg_config/nvim/lua/jedn/plugins/nvim-tree.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  depencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   cmd = { "NvimTreeToggle", "NvimTreeFocus" },
 
   opts = {
