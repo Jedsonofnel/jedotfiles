3c3d7a93d2f8addd479af65b1654b622975b95a3
Jed Nelson <jed@nelson.ac>
2026-07-22 11:39:24 +0100

    Clean up paredit.fnl

--- xdg_config/nvim/after/plugin/paredit.fnl
+++ xdg_config/nvim/after/plugin/paredit.fnl
@@ -1,32 +1,53 @@
 ;; some nvim-paredit config
 
 (local paredit (require :nvim-paredit))
-(local {: api} paredit)
+(local {: api : cursor : wrap} paredit)
 
-(fn wrap-parent [l-delim r-delim]
-  "Creates function for wrapping form with delim"
-  (fn []
-    (api.wrap_enclosing_form_under_cursor {:opener l-delim :closer r-delim})))
+(local closing {"(" ")" "[" "]" "{" "}"})
+(local placement-str {:inner_start :head :inner_end :tail})
 
-(fn wrap-elem [l-delim r-delim]
-  "Creates function for wrapping element with delim"
-  (fn []
-    (api.wrap_enclosing_form_under_cursor {:opener l-delim :closer r-delim})))
+(fn wrap-parent [delim placement]
+  "Creates function for wrapping form with delim, insert at placement"
+  (let [endelim (. closing delim)
+        placement (or placement :inner_start)]
+    [(fn []
+       (cursor.place_cursor (wrap.wrap_enclosing_form_under_cursor delim
+                                                                   endelim)
+                            {: placement :mode :insert}))
+     (string.format "Wrap form with %s%s, insert %s" delim endelim
+                    (. placement-str placement))]))
+
+(fn wrap-parent-tail [delim]
+  (wrap-parent delim :inner_start))
+
+(fn wrap-elem [delim placement]
+  "Creates function for wrapping element with delim, insert at placement"
+  (let [endelim (. closing delim)
+        placement (or placement :inner_start)]
+    [(fn []
+       (cursor.place_cursor (wrap.wrap_element_under_cursor delim endelim)
+                            {: placement :mode :insert}))
+     (string.format "Wrap element with %s%s, insert %s" delim endelim
+                    (. placement-str placement))]))
+
+(fn wrap-elem-tail [delim]
+  (wrap-elem delim :inner_end))
 
 (local keys {;; Defaults are automatically included
              ;; Wrap compound form
-             :<localleader>i [(wrap-parent "(" ")") "Wrap form with ()"]
-             "<localleader>[" [(wrap-parent "[" "]") "Wrap form with []"]
-             "<localleader>{" [(wrap-parent "{" "}") "Wrap form with {}"]
+             :<localleader>i (wrap-parent "(")
+             :<localleader>I (wrap-parent-tail "(")
+             "<localleader>[" (wrap-parent "[")
+             "<localleader>]" (wrap-parent-tail "[")
+             "<localleader>{" (wrap-parent "{")
+             "<localleader>}" (wrap-parent-tail "{")
              ;; Wrap element
-             :<localleader>w [(wrap-elem "(" ")") "Wrap element with ()"]
-             "<localleader>e[" [(wrap-elem "[" "]") "Wrap element with []"]
-             "<localleader>e{" [(wrap-elem "{" "}") "Wrap element with {}"]
-             ;; M-hjkl drag bindings
-             :<M-l> [api.drag_element_forwards "Drag element right"]
-             :<M-h> [api.drag_element_backwards "Drag element left"]
-             :<M-k> [api.drag_form_forwards "Drag form right"]
-             :<M-j> [api.drag_form_backwards "Drag form left"]
+             :<localleader>w (wrap-elem "(")
+             :<localleader>W (wrap-elem-tail "(")
+             "<localleader>e[" (wrap-elem "[")
+             "<localleader>e]" (wrap-elem-tail "[")
+             "<localleader>e{" (wrap-elem "{" "}")
+             "<localleader>e}" (wrap-elem-tail "{")
              ;; M-S-hjkl slurp/barf (vim-sexp style)
              :<M-S-l> [api.slurp_forwards "Slurp forwards"]
              :<M-S-h> [api.barf_forwards "Barf forwards"]
--- xdg_config/nvim/after/plugin/paredit.lua
+++ xdg_config/nvim/after/plugin/paredit.lua
@@ -1,17 +1,31 @@
 -- [nfnl] after/plugin/paredit.fnl
 local paredit = require("nvim-paredit")
 local api = paredit.api
-local function wrap_parent(l_delim, r_delim)
+local cursor = paredit.cursor
+local wrap = paredit.wrap
+local closing = {["("] = ")", ["["] = "]", ["{"] = "}"}
+local placement_str = {inner_start = "head", inner_end = "tail"}
+local function wrap_parent(delim, placement)
+  local endelim = closing[delim]
+  local placement0 = (placement or "inner_start")
   local function _1_()
-    return api.wrap_enclosing_form_under_cursor({opener = l_delim, closer = r_delim})
+    return cursor.place_cursor(wrap.wrap_enclosing_form_under_cursor(delim, endelim), {placement = placement0, mode = "insert"})
   end
-  return _1_
+  return {_1_, string.format("Wrap form with %s%s, insert %s", delim, endelim, placement_str[placement0])}
 end
-local function wrap_elem(l_delim, r_delim)
+local function wrap_parent_tail(delim)
+  return wrap_parent(delim, "inner_start")
+end
+local function wrap_elem(delim, placement)
+  local endelim = closing[delim]
+  local placement0 = (placement or "inner_start")
   local function _2_()
-    return api.wrap_enclosing_form_under_cursor({opener = l_delim, closer = r_delim})
+    return cursor.place_cursor(wrap.wrap_element_under_cursor(delim, endelim), {placement = placement0, mode = "insert"})
   end
-  return _2_
+  return {_2_, string.format("Wrap element with %s%s, insert %s", delim, endelim, placement_str[placement0])}
+end
+local function wrap_elem_tail(delim)
+  return wrap_elem(delim, "inner_end")
 end
 local keys
 local function _3_()
@@ -22,5 +36,5 @@ local function _4_()
   api.move_to_parent_form_end()
   return vim.cmd("startinsert")
 end
-keys = {["<localleader>i"] = {wrap_parent("(", ")"), "Wrap form with ()"}, ["<localleader>["] = {wrap_parent("[", "]"), "Wrap form with []"}, ["<localleader>{"] = {wrap_parent("{", "}"), "Wrap form with {}"}, ["<localleader>w"] = {wrap_elem("(", ")"), "Wrap element with ()"}, ["<localleader>e["] = {wrap_elem("[", "]"), "Wrap element with []"}, ["<localleader>e{"] = {wrap_elem("{", "}"), "Wrap element with {}"}, ["<M-l>"] = {api.drag_element_forwards, "Drag element right"}, ["<M-h>"] = {api.drag_element_backwards, "Drag element left"}, ["<M-k>"] = {api.drag_form_forwards, "Drag form right"}, ["<M-j>"] = {api.drag_form_backwards, "Drag form left"}, ["<M-S-l>"] = {api.slurp_forwards, "Slurp forwards"}, ["<M-S-h>"] = {api.barf_forwards, "Barf forwards"}, ["<M-S-k>"] = {api.slurp_backwards, "Slurp backwards"}, ["<M-S-j>"] = {api.barf_backwards, "Barf backwards"}, ["<localleader>^"] = {_3_, "Insert at form head"}, ["<localleader>$"] = {_4_, "Insert at form tail"}}
+keys = {["<localleader>i"] = wrap_parent("("), ["<localleader>I"] = wrap_parent_tail("("), ["<localleader>["] = wrap_parent("["), ["<localleader>]"] = wrap_parent_tail("["), ["<localleader>{"] = wrap_parent("{"), ["<localleader>}"] = wrap_parent_tail("{"), ["<localleader>w"] = wrap_elem("("), ["<localleader>W"] = wrap_elem_tail("("), ["<localleader>e["] = wrap_elem("["), ["<localleader>e]"] = wrap_elem_tail("["), ["<localleader>e{"] = wrap_elem("{", "}"), ["<localleader>e}"] = wrap_elem_tail("{"), ["<M-S-l>"] = {api.slurp_forwards, "Slurp forwards"}, ["<M-S-h>"] = {api.barf_forwards, "Barf forwards"}, ["<M-S-k>"] = {api.slurp_backwards, "Slurp backwards"}, ["<M-S-j>"] = {api.barf_backwards, "Barf backwards"}, ["<localleader>^"] = {_3_, "Insert at form head"}, ["<localleader>$"] = {_4_, "Insert at form tail"}}
 return paredit.setup({use_default_keys = true, keys = keys})
